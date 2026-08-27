b09f8a017df51dbaebb39853ac988297d2c59816
ahmedelomda777 <ahmedabs777@gmail.com>
2026-08-28 00:30:42 +0300

    add navbar and hero

---
+++ components/layout/navbar.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { useEffect, useState } from "react";
+import { navData } from "@/data/data";
+
+export default function Navbar() {
+  const [activeSection, setActiveSection] = useState("/");
+
+  useEffect(() => {
+    const sections = navData
+      .filter((n) => n.href !== "/")
+      .map((n) => document.querySelector(n.href));
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+          console.log("active:", entry.target.id); // ← ضيف السطر ده
+            setActiveSection(`#${entry.target.id}`);
+          }
+        });
+      },
+      { rootMargin: "-50% 0px -50% 0px" } 
+    );
+
+    sections.forEach((section) => section && observer.observe(section));
+
+    return () => observer.disconnect();
+  }, []);
+
+  return (
+    <nav className="sticky z-50 top-1 w-9/10 m-auto mt-5 mb-5 p-3 bg-white/10 backdrop-blur-xl border border-white/20 rounded-2xl">
+      <ul className="flex items-center justify-around list-none">
+        {navData.map((n) => {
+          const isActive = activeSection === n.href;
+          return (
+            <li
+              className={`text-primary-text ${isActive ? "border-b-2 border-primary pb-2" : ""}`}
+              key={n.name}
+            >
+              <a className="flex items-center gap-2" href={n.href}>
+                {n.name}
+                <n.icon className="w-5 h-5" />
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+}
---
+++ data/data.ts
@@ -0,0 +1,23 @@
+import { HomeIcon, User, LayoutDashboard, Mail } from "lucide-react";
+export const navData = [
+  {
+    name: "Home",
+    href: "#home",
+    icon: HomeIcon,
+  },
+  {
+    name: "About me",
+    href: "#about",
+    icon: User,
+  },
+  {
+    name: "Projects",
+    href: "#projects",
+    icon: LayoutDashboard,
+  },
+  {
+    name: "Contact",
+    href: "#contact",
+    icon: Mail,
+  },
+];
